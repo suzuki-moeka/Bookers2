0efb02a6c890d098d2eccb4b314f642122dacdc6
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-07-18 04:12:58 +0000

    [finish] 4-userのshowページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def top
   end
+
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+ def update
+    @user = find(params[:id])
+    @user.update
+    redirect_to user_path(user.id)
+ end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
