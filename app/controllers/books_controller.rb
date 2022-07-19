e8d972067d000c1803c6ac584761290660021c1d
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-07-19 13:59:12 +0000

    [finish] 4-フラッシュメッセージ設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,16 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path(@book.id)
+    if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to books_path(@book.id)
+    else
+      flash[:alret] = "error Posting failed"
+      @books = Book.all
+      render "index"
+    end
   end
 
   def index
@@ -28,8 +35,14 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      flash[:alret] = "error Update failed"　
+      @books = Book.all
+      render "index"
+    end
   end
 
   def destroy
